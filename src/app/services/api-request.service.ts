87ea250fc9e49b5bd6833db0e99584925d65e7e4
Akram Qabodan <akramqabodan22@gmail.com>
2022-07-04 15:45:59 +0200

    set layout

---
+++ src/app/services/api-request.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiRequestService {
+  key: string = '6e17bd4768b3f5848c1d3b05fd8cadd9';
+  constructor() {}
+}
