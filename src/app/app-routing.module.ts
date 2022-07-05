4463d33b9f2402fbac98bd7ad6008e4d4edaa37a
nadayouseryabdo <nada.yousery.abdo@gmail.com>
2022-07-05 11:02:05 +0200

    cover&routes&navs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoverComponent } from './components/cover/cover.component';
-import { HomeComponent } from './components/home/home.component';
-import { PopularPeopleComponent } from './components/home/popular-people/popular-people.component';
-import { TrendingMoviesComponent } from './components/home/trending-movies/trending-movies.component';
-import { TrendingTvComponent } from './components/home/trending-tv/trending-tv.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {path:'',component:CoverComponent}, 
-  {path:'home',component: HomeComponent},
-  {path:'trendMovies',component: TrendingMoviesComponent},
-  {path:'trendTv',component:TrendingTvComponent},
-  {path:'trendPeople',component: PopularPeopleComponent},
-  {path:'auth',loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)}
+  {path:'auth',loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)},
+  {path:'home',loadChildren:()=>import('./home-page/home-page.module').then(m=>m.HomePageModule)},
+  {path:'**',component: NotFoundComponent},
 ];
 
 @NgModule({
