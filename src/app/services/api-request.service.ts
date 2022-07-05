d1e610ab0a3b001b20ea0cabf89e42a12c7259fe
Akram Qabodan <akramqabodan22@gmail.com>
2022-07-05 16:11:10 +0200

    remove subscribe from service && modify img URL

--- src/app/services/api-request.service.ts
+++ src/app/services/api-request.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiRequestService {
   key: string = '6e17bd4768b3f5848c1d3b05fd8cadd9';
+  baseImgURL: string = 'https://image.tmdb.org/t/p';
+  imgSize: string = '/original';
+  trendingPeople: any = new BehaviorSubject([]);
   data: { trendingPeople: {}[]; trendingMovies: {}[]; trendingTV: {}[] } = {
     trendingPeople: [],
     trendingMovies: [],
@@ -15,47 +18,50 @@ export class ApiRequestService {
   constructor(private http: HttpClient) {}
 
   getTrendingPeople() {
-    this.http
+    return this.http
       .get(
         `https://api.themoviedb.org/3/trending/person/day?api_key=${this.key}`
       )
-      .pipe(map((data: any) => data.results))
-      .subscribe(
-        (data) => {
-          this.data.trendingPeople = data;
-          console.log(data);
-        },
-        (error) => {
-          alert('failed to load data! please try again later');
-        }
+      .pipe(
+        map((data: any) => data.results),
+        map((item) => {
+          return item.map((movie: any) => {
+            return {
+              ...movie,
+              profile_path: `${this.baseImgURL}${this.imgSize}${movie.profile_path}`,
+            };
+          });
+        })
       );
   }
   getTrendingMovies() {
-    this.http
+    return this.http
       .get(`https://api.themoviedb.org/3/trending/tv/day?api_key=${this.key}`)
-      .pipe(map((data: any) => data.results))
-      .subscribe(
-        (data) => {
-          this.data.trendingPeople = data;
-          console.log(data);
-        },
-        (error) => {
-          alert('failed to load data! please try again later');
-        }
+      .pipe(
+        map((data: any) => data.results),
+        map((item) => {
+          return item.map((movie: any) => {
+            return {
+              ...movie,
+              poster_path: `${this.baseImgURL}${this.imgSize}${movie.poster_path}`,
+            };
+          });
+        })
       );
   }
   getTrendingTV() {
-    this.http
+    return this.http
       .get(`https://api.themoviedb.org/3/trending/tv/day?api_key=${this.key}`)
-      .pipe(map((data: any) => data.results))
-      .subscribe(
-        (data) => {
-          this.data.trendingPeople = data;
-          console.log(data);
-        },
-        (error) => {
-          alert('failed to load data! please try again later');
-        }
+      .pipe(
+        map((data: any) => data.results),
+        map((item) => {
+          return item.map((tv: any) => {
+            return {
+              ...tv,
+              poster_path: `${this.baseImgURL}${this.imgSize}${tv.poster_path}`,
+            };
+          });
+        })
       );
   }
 }
