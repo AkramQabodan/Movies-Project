8319b87f3b9179c633097f298bef11e98f38aaaa
Ranahossamsalah <rana.hossam.salah2017@gmail.com>
2022-07-06 16:06:17 +0200

    adding popular people url and service in ts

--- src/app/home-page/popular-people/popular-people.component.ts
+++ src/app/home-page/popular-people/popular-people.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { popular } from 'app/interfaces/popularinteface';
 import { ApiRequestService } from 'app/services/api-request.service';
-
+// import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-popular-people',
   templateUrl: './popular-people.component.html',
-  styleUrls: ['./popular-people.component.scss']
+  styleUrls: ['./popular-people.component.scss'],
 })
 export class PopularPeopleComponent implements OnInit {
-  trendingpeople:Array<popular>=[]
-  
-  constructor( private _ApiRequestService:ApiRequestService
-  ) { }
-  ngOnInit(): void {
-    this._ApiRequestService.getTrendingPeople()
-    
-  }
+  trendingpeople: Array<popular> = [];
 
+  people:Array<popular> =[]
+  constructor(
+    private _ApiRequestService: ApiRequestService,
+    // private _route: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    // this._route.params.subscribe(params =>{
+    //   if (params.searchterm) 
+    //   this.people = this.trendingpeople.getAll()
+    // })
+    this._ApiRequestService.getTrendingPeople().subscribe(
+      (res) => {
+        console.log('res');
+        for (const product of res) {
+          this.trendingpeople.push(product);
+        }
+      },
+
+      (err: any) => {
+        console.log('error');
+      }
+    );
+  }
 }
